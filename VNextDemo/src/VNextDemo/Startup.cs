2ffd2308f02ca3cf2a5d01336f8e6810953af3bf
CodeworxSamples <raphael@codeworx.at>
2015-06-19 03:08:15 +0200

    Config

--- VNextDemo/src/VNextDemo/Startup.cs
+++ VNextDemo/src/VNextDemo/Startup.cs
@@ -8,20 +8,26 @@ using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
 using Swashbuckle.Application;
 using Swashbuckle.Swagger;
+using Microsoft.Framework.ConfigurationModel;
 
 namespace VNextDemo
 {
     public class Startup
     {
+        public static IConfiguration Config;
+
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            Config = new Configuration()
+                        .AddJsonFile("config.json")
+                        .AddEnvironmentVariables();
+
             services.AddMvc();
             services.AddEntityFramework()
                 .AddSqlServer()
-                .AddDbContext<BookContext>(options =>
-                    options
-                    .UseSqlServer("connstr"));
+                .AddDbContext<BookContext>(options => options
+                                .UseSqlServer(Config.Get("Data:DefaultConnection:ConnectionString")));
 
             services.AddSwagger(s => {
                 s.SwaggerGenerator(c => {
